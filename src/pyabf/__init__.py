7bb257f99d9cea85a910cae3e8ee7b275bead36b
Scott W Harden <swharden@gmail.com>
2017-11-18 09:46:43 -0500

    pypi release 0.1.4
    now supports multi-channel ABFs and ABF1 files

--- src/pyabf/__init__.py
+++ src/pyabf/__init__.py
@@ -5,7 +5,7 @@ Documentation, code examples, and more can be found at: https://github.com/swhar
 
 from pyabf.abf import ABF
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 def info():
     """display information about the pyabf package."""
