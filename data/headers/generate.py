90d60b3858f24546415ce01f493d568a279d42b0
Scott W Harden <swharden@gmail.com>
2017-11-18 13:25:03 -0500

    automatic header readme links

--- data/headers/generate.py
+++ data/headers/generate.py
@@ -5,10 +5,17 @@ sys.path.append("../../src/")
 sys.path.insert(0,sys.path.pop())
 import pyabf
 import glob
+import os
 
 if __name__=="__main__":
-    for fname in glob.glob("../*.abf"):
+    out="# Sample ABF Header Data\n"
+    out+="This folder contains automatically-generated header summaries of all ABFs in the data folder.\n\n"
+    for fname in sorted(glob.glob("../*.abf")):
         print(fname)
         abf=pyabf.ABF(fname)
         abf._abfHeader.saveHTML(abf.ID+".html")
         abf._abfHeader.saveMarkdown(abf.ID+".md")
+        out+="* [%s](%s)\n"%(os.path.basename(abf.filename),abf.ID+".md")
+    with open('readme.md','w') as f:
+        f.write(out)
+    print("updated readme.md")
