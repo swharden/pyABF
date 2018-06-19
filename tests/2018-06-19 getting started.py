46d6c762f67c17de21491830b8020c4dafebf9b8
Scott W Harden <swharden@gmail.com>
2018-06-19 11:58:37 -0400

    first test

--- tests/2018-06-19 getting started.py
+++ tests/2018-06-19 getting started.py
@@ -12,12 +12,15 @@ sys.path.insert(0,PATH_SRC) # for importing
 sys.path.append("../src/") # for your IDE
 import pyabf
 pyabf.info()
+import datetime
+import numpy as np
 
 # now you are free to import additional modules
 import glob
 
 if __name__=="__main__":
-    for fname in sorted(glob.glob(PATH_DATA+"/*.abf")):
+    for fname in sorted(glob.glob(PATH_DATA+"/*.abf"))[4:5]:
         abf = pyabf.ABF(fname)
-        print(abf.abfID, abf.dataChannelCount)
+        page = abf.infoPage()
+        page.launchTempWebpage()
     print("DONE")
