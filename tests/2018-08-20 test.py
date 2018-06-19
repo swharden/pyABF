8aea849f01cd430e88da7c034a8dc4feaaed8649
Scott W Harden <swharden@gmail.com>
2018-06-19 13:02:58 -0400

    new test

---
+++ tests/2018-08-20 test.py
@@ -0,0 +1,27 @@
+"""
+Example code demonstrating how to interact with pyabf
+"""
+
+# import the pyabf module from this development folder
+import os
+import sys
+PATH_HERE = os.path.abspath(os.path.dirname(__file__))
+PATH_SRC = os.path.abspath(PATH_HERE+"/../src/")
+PATH_DATA = os.path.abspath(PATH_HERE+"/../data/")
+sys.path.insert(0,PATH_SRC) # for importing
+sys.path.append("../src/") # for your IDE
+import pyabf
+pyabf.info()
+import datetime
+import numpy as np
+
+# now you are free to import additional modules
+import glob
+
+if __name__=="__main__":
+
+    for fname in sorted(glob.glob(PATH_DATA+"/*.abf")):
+        abf = pyabf.ABF(fname)
+        print(abf.abfID)
+        page = abf.getInfoPage()
+    print("DONE")
