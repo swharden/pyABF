9cbcbeff9404576855ee7d28d750f08c7966e7be
Scott W Harden <swharden@gmail.com>
2018-06-19 18:15:32 -0400

    improve thumbnail transparency

--- tests/2018-06-19 header index update.py
+++ tests/2018-06-19 header index update.py
@@ -34,40 +34,43 @@ if __name__ == "__main__":
 
     # clear everything that used to be in the headers folder
     for fname in glob.glob(PATH_DATA+"/headers/*.*"):
-        print("deleting", fname,"...")
+        print("deleting", fname, "...")
         os.remove(fname)
 
     for fname in sorted(glob.glob(PATH_DATA+"/abfs/*.abf")):
-        abf=pyabf.ABF(fname)
-        print("processing",abf.abfID,"...")
+        abf = pyabf.ABF(fname)
+        print("processing", abf.abfID, "...")
 
         # create info pages
-        page=abf.getInfoPage()
-        headerPath=os.path.abspath(PATH_HERE+"/../data/headers/")
+        page = abf.getInfoPage()
+        headerPath = os.path.abspath(PATH_HERE+"/../data/headers/")
         with open(headerPath+"/%s.md" % (abf.abfID), 'w') as f:
             f.write(page.generateMarkdown())
         with open(headerPath+"/%s.html" % (abf.abfID), 'w') as f:
             f.write(page.generateHTML())
 
         # create figure and subplots
-        fig=plt.figure(figsize = (8, 6))
-        ax1=plt.subplot(211)
-        ax2=plt.subplot(212)
+        fig = plt.figure(figsize=(8, 6))
+        fig.patch.set_alpha(0)  # transparent background
+        ax1 = plt.subplot(211)
+        ax2 = plt.subplot(212)
+        ax1.patch.set_facecolor('w')
+        ax2.patch.set_facecolor('w')
         ax1.set_xmargin(0)
         ax2.set_xmargin(0)
 
         # overlap or continuous view depends on protocol
-        absoluteTime=False
+        absoluteTime = False
         if "0111" in abf.protocol:
-            absoluteTime=True
+            absoluteTime = True
         if "loose" in abf.protocol:
-            absoluteTime=True
+            absoluteTime = True
 
         # plot the data sweep by sweep
         for sweep in abf.sweepList:
-            abf.setSweep(sweep, absoluteTime = absoluteTime)
-            ax1.plot(abf.sweepX, abf.sweepY, alpha = .5, color = 'b', lw = .5)
-            ax2.plot(abf.sweepX, abf.sweepC, color = 'r')
+            abf.setSweep(sweep, absoluteTime=absoluteTime)
+            ax1.plot(abf.sweepX, abf.sweepY, alpha=.5, color='b', lw=.5)
+            ax2.plot(abf.sweepX, abf.sweepC, color='r')
 
         # decorate plot and save it
         ax1.set_title("{}.abf [channel: {}/{}] [sweeps: {}]".format(
@@ -77,14 +80,12 @@ if __name__ == "__main__":
         ax2.set_ylabel(abf.sweepLabelC)
         ax2.set_xlabel(abf.sweepLabelX)
         fig.tight_layout()
-        fig.savefig(PATH_HERE+"/../data/headers/%s.png" %
-                    abf.abfID, transparent = True)
+        fig.savefig(PATH_HERE+"/../data/headers/%s.png" % abf.abfID)
         plt.close()
-
 
         # update main readme
         md += "[%s.abf](headers/%s.md) (%s)|" % (abf.abfID,
-                        abf.abfID, abf.abfVersion)
+                                                 abf.abfID, abf.abfVersion)
         md += "%s (%s)|" % (abf.channelCount, ", ".join(abf.adcUnits))
         md += "%s|" % (abf.sweepCount)
         md += "%s|" % (abf.protocol)
