swharden/pyABF

commit 9bbf45bd73c16a4b28642ae75bd3a2935dfab14b

protocol generation

src/pyabf/core.py
@@ -50,7 +50,8 @@ class ABFcore:
         This used to be the __init__ of the ABF class.
         """
         self.abfFilePath = os.path.abspath(abf)
-        assert os.path.exists(self.abfFilePath)
+        if not os.path.exists(self.abfFilePath):
+            raise ValueError("file does not exist", self.abfFilePath)
         self.abfID = os.path.splitext(os.path.basename(self.abfFilePath))[0]
         self._fileOpen()
         self._determineAbfFormat()
@@ -64,6 +65,7 @@ class ABFcore:
         self._determineProtocolPath()
         self._determineProtocolComment()
         self._makeTagTimesHumanReadable()
+        self._makeUsefulObjects()
         if preLoadData:
             self._loadAndScaleData()
         self._fileClose()
@@ -168,19 +170,19 @@ class ABFcore:
             self.dataByteStart = self._headerV1.lDataSectionPtr*512
             self.dataByteStart += self._headerV1.nNumPointsIgnored
             self.dataPointCount = self._headerV1.lActualAcqLength
-            self.dataChannelCount = self._headerV1.nADCNumChannels
+            self.channelCount = self._headerV1.nADCNumChannels
             self.dataRate = int(1e6 / self._headerV1.fADCSampleInterval)
             self.dataSecPerPoint = 1/self.dataRate
             self.sweepCount = self._headerV1.lActualEpisodes
             if self.sweepCount == 0:  # gap free file
                 self.sweepCount = 1
             self.sweepPointCount = int(self.dataPointCount / self.sweepCount)
-            self.sweepPointCount = int(self.sweepPointCount / self.dataChannelCount)
+            self.sweepPointCount = int(self.sweepPointCount / self.channelCount)
             self.sweepLengthSec = self.sweepPointCount / self.dataRate
         elif self.abfFileFormat == 2:
             self.dataByteStart = self._sectionMap.DataSection[0]*512
             self.dataPointCount = self._sectionMap.DataSection[2]
-            self.dataChannelCount = self._sectionMap.ADCSection[2]
+            self.channelCount = self._sectionMap.ADCSection[2]
             self.dataRate = int(
                 1e6 / self._protocolSection.fADCSequenceInterval)
             self.dataSecPerPoint = 1/self.dataRate
@@ -188,7 +190,7 @@ class ABFcore:
             if self.sweepCount == 0:  # gap free file
                 self.sweepCount = 1
             self.sweepPointCount = int(self.dataPointCount / self.sweepCount)
-            self.sweepPointCount = int(self.sweepPointCount / self.dataChannelCount)
+            self.sweepPointCount = int(self.sweepPointCount / self.channelCount)
             self.sweepLengthSec = self.sweepPointCount / self.dataRate
         else:
             raise NotImplementedError("Invalid ABF file format")
@@ -200,15 +202,15 @@ class ABFcore:
         organizes channel units and names into simple lists of strings.
         """
         if self.abfFileFormat == 1:
-            self.adcUnits = self._headerV1.sADCUnits[:self.dataChannelCount]
-            self.adcNames = self._headerV1.sADCChannelName[:self.dataChannelCount]
+            self.adcUnits = self._headerV1.sADCUnits[:self.channelCount]
+            self.adcNames = self._headerV1.sADCChannelName[:self.channelCount]
             self.dacUnits = ["?" for x in self.adcUnits]
             self.dacNames = ["?" for x in self.adcUnits]
         elif self.abfFileFormat == 2:
-            self.adcUnits = self._stringsIndexed.lADCUnits[:self.dataChannelCount]
-            self.adcNames = self._stringsIndexed.lADCChannelName[:self.dataChannelCount]
-            self.dacUnits = self._stringsIndexed.lDACChannelUnits[:self.dataChannelCount]
-            self.dacNames = self._stringsIndexed.lDACChannelName[:self.dataChannelCount]
+            self.adcUnits = self._stringsIndexed.lADCUnits[:self.channelCount]
+            self.adcNames = self._stringsIndexed.lADCChannelName[:self.channelCount]
+            self.dacUnits = self._stringsIndexed.lDACChannelUnits[:self.channelCount]
+            self.dacNames = self._stringsIndexed.lDACChannelName[:self.channelCount]
         else:
             raise NotImplementedError("Invalid ABF file format")
 
@@ -220,12 +222,12 @@ class ABFcore:
         reads the header to determine how to scale each channel.
         """
         if self.abfFileFormat == 1:
-            self.scaleFactors = [1]*self.dataChannelCount
-            for i in range(self.dataChannelCount):
+            self.scaleFactors = [1]*self.channelCount
+            for i in range(self.channelCount):
                 self.scaleFactors[i] = self._headerV1.lADCResolution/1e6
         elif self.abfFileFormat == 2:
-            self.scaleFactors = [1]*self.dataChannelCount
-            for i in range(self.dataChannelCount):
+            self.scaleFactors = [1]*self.channelCount
+            for i in range(self.channelCount):
                 self.scaleFactors[i] /= self._adcSection.fInstrumentScaleFactor[i]
                 self.scaleFactors[i] /= self._adcSection.fSignalGain[i]
                 self.scaleFactors[i] /= self._adcSection.fADCProgrammableGain[i]
@@ -314,10 +316,10 @@ class ABFcore:
         self._fb.seek(self.dataByteStart)
         raw = np.fromfile(self._fb, dtype=np.int16, count=self.dataPointCount)
         raw = np.reshape(
-            raw, (int(len(raw)/self.dataChannelCount), self.dataChannelCount))
+            raw, (int(len(raw)/self.channelCount), self.channelCount))
         raw = np.rot90(raw)
         self.data = np.empty(raw.shape, dtype='float32')
-        for i in range(self.dataChannelCount):
+        for i in range(self.channelCount):
             self.data[i] = np.multiply(
                 raw[i], self.scaleFactors[i], dtype='float32')
 
@@ -343,8 +345,12 @@ class ABFcore:
                 if thingName.startswith("_"):
                     continue
                 thing = getattr(self, thingName)
-                if isinstance(thing, (int, list, float, datetime.datetime, str, np.ndarray)):
+                if "method" in str(type(thing)):
+                    continue
+                if isinstance(thing, (int, list, float, datetime.datetime, str, np.ndarray, range)):
                     page.addThing(thingName, thing)
+                else:
+                    print("Unsure how to generate infor for:", thingName, type(thing))
 
             # add all ABF header information (different in ABF1 vs ABF2)
 
@@ -372,3 +378,82 @@ class ABFcore:
                     page.addThing(subItemName, getattr(thingItself, subItemName))
 
             return page
+
+    def _makeUsefulObjects(self):
+        """
+        Create a few extra little objects which are useful when working with
+        the abf object.
+        """
+        self.channelList = list(range(self.channelCount))
+        self.sweepList = list(range(self.sweepCount))
+
+    def _updateStimulusWaveform(self, sweepNumber, channel):
+        """
+        Update self.sweepC with the stimulus waveform for a given sweep.
+        Epoch information is stored in EpochPerDACSection.
+        """
+
+        # known epoch types
+        epoch_disabled = 0
+        epoch_step = 1
+        epoch_ramp = 2
+
+        # create the waveform and pre-fill it entirely with the holding value
+        self.sweepC = np.full(self.sweepPointCount, self.holdingCommand[channel])
+
+        # this function only supports ABF2 formatted files
+        if self.abfFileFormat != 2:
+            return
+
+        # it's just holding through 1/64th of the sweep length (why?!?)
+        position = int(self.sweepPointCount/64)
+
+        # update the waveform for each epoch
+        for epochNumber, epochType in enumerate(self._epochPerDacSection.nEpochType):
+            pointCount = self._epochPerDacSection.lEpochInitDuration[epochNumber]
+            deltaCommand = self._epochPerDacSection.fEpochLevelInc[epochNumber] * sweepNumber
+            deltaCommandLast = self._epochPerDacSection.fEpochLevelInc[epochNumber] * (sweepNumber-1)
+            thisCommand = self._epochPerDacSection.fEpochInitLevel[epochNumber]
+            afterDelta = thisCommand+deltaCommand
+            position2 = position + pointCount
+
+            # if nInterEpisodeLevel start from the last command
+            if self._dacSection.nInterEpisodeLevel[channel]:
+                self.sweepC = np.full(self.sweepPointCount, thisCommand+deltaCommandLast)
+                
+            # do something different depending on what type of epoch it is
+            if epochType == epoch_disabled:
+                continue
+
+            elif epochType == epoch_step:
+                self.sweepC[position:position2] = afterDelta
+
+            elif epochType == epoch_ramp:
+                ramp = np.arange(pointCount)/pointCount
+                rampStart = self.sweepC[position-1]
+                rampEnd = afterDelta
+                rampDiff = rampEnd-rampStart
+                ramp *= rampDiff
+                ramp += rampStart
+                self.sweepC[position:position2] = ramp
+
+            else:
+                warnings.warn(
+                    "treating unknown epoch type like a step", epochType)
+                self.sweepC[position:position2] = afterDelta
+            position += pointCount
+
+        # if nInterEpisodeLevel sustain the last command (ignore holding)
+        if self._dacSection.nInterEpisodeLevel[channel]:
+            self.sweepC[position2:] = afterDelta
+
+    def _commandContainsDeltas(self):
+        """
+        returns True if sweeps contain deltas.
+        """
+        if self.abfFileFormat==1:
+            return False
+        if np.any(self._epochPerDacSection.fEpochLevelInc):
+            return True
+        else:
+            return False

src/pyabf/abf.py
@@ -2,11 +2,15 @@
 Code in this file provides high-level ABF interactivity.
 Things the user will interact with directly are in this file.
 """
-
+import sys
 import glob
 import datetime
 import numpy as np
-import matplotlib.pyplot as plt
+import warnings
+
+if __name__ == "__main__":
+    warnings.warn("DO NOT RUN THIS FILE DIRECTLY! It is for testing only.")
+    sys.path.append(".")
 
 from pyabf.core import ABFcore
 
@@ -28,24 +32,76 @@ class ABF(ABFcore):
         self._loadEverything(abf, preLoadData)
         self.setSweep(0)
 
-    def setSweep(self, sweepNumber, channel=0):
+    def setSweep(self, sweepNumber, channel=0, absoluteTime=False):
 
-        # ensure the sweep number is valid
-        assert sweepNumber>=0
-        assert sweepNumber<self.sweepCount
+        # sweep number error checking
+        while sweepNumber < 0:
+            sweepNumber = self.sweepCount - sweepNumber
+        if sweepNumber >= self.sweepCount:
+            warnings.warn(
+                "Requested sweep is out of bounds. Using last sweep.")
+            sweepNumber = self.sweepCount
 
-        # ensure channel number is valid
-        assert channel>=0
-        assert channel<self.dataChannelCount
+        # channel error checking
+        if channel < 0 or channel >= self.channelCount:
+            warnings.warn(
+                "Requested channel is out of bounds. Using first channel.")
 
         # determine data bounds for that sweep
         pointStart = self.sweepPointCount*sweepNumber
         pointEnd = pointStart + self.sweepPointCount
 
-        self.sweep = Sweep()
-        self.sweep.units = self.adcUnits[channel]
-        self.sweep.unitsClamp = self.dacUnits[channel]
-        self.sweep.number = sweepNumber
-        self.sweep.channel = channel
-        self.sweep.y = self.data[channel, pointStart:pointEnd]
-        self.sweep.x = np.arange(len(self.sweep.y))*self.dataSecPerPoint
+        # start updating class-level variables
+
+        # sweep information
+        self.sweepNumber = sweepNumber
+        self.sweepChannel = channel
+        self.sweepUnitsY = self.adcUnits[channel]
+        self.sweepUnitsC = self.dacUnits[channel]
+        self.sweepUnitsX = "sec"
+
+        # standard labels
+        self.sweepLabelY = "{} ({})".format(
+            self.adcNames[channel], self.adcUnits[channel])
+        self.sweepLabelC = "{} ({})".format(
+            self.dacNames[channel], self.dacUnits[channel])
+        self.sweepLabelX = "time (seconds)"
+
+        # use fancy labels for known units
+        if self.sweepUnitsY == "pA":
+            self.sweepLabelY = "Clamp Current (pA)"
+            self.sweepLabelC = "Membrane Potential (mV)"
+        elif self.sweepUnitsY == "mV":
+            self.sweepLabelY = "Membrane Potential (mV)"
+            self.sweepLabelC = "Applied Current (pA)"
+
+        # load the actual sweep data
+        self.sweepY = self.data[channel, pointStart:pointEnd]
+        self.sweepX = np.arange(len(self.sweepY))*self.dataSecPerPoint
+        if absoluteTime:
+            self.sweepX += sweepNumber * self.sweepLengthSec
+        self._updateStimulusWaveform(sweepNumber, channel)
+
+
+
+
+# developer sandbox
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    for fname in sorted(glob.glob("../data/*.abf")):
+        abf = ABF(fname)
+        print(abf.abfID, abf._commandContainsDeltas())
+
+    # abf = ABF("../data/171116sh_0018.abf")
+
+    # fig = plt.figure()
+    # ax1 = fig.add_subplot(211)
+    # ax2 = fig.add_subplot(212, sharex=ax1)
+    # for sweep in abf.sweepList:
+    #     abf.setSweep(sweep, absoluteTime=True)
+    #     ax1.plot(abf.sweepX, abf.sweepY, color='b')
+    #     ax2.plot(abf.sweepX, abf.sweepC, color='r')
+    # plt.show()
+
+    print("DONE")

src/pyabf/text.py
@@ -5,6 +5,38 @@ Code here relates to text parsing and other ninjary
 import tempfile
 import webbrowser
 import time
+import glob
+import os
+
+
+def indexFolder(folder, launch=True):
+    html = "<html><head><style>"
+    html+="body {background-color: #ddd;}"
+    html+="img {border: 1px solid black; margin: 20px;}"
+    html+="img {box-shadow: 5px 5px 15px rgba(0, 0, 0, .3);}"
+    html+="img {height: 300px;}"
+    html+="</style></head><body>"
+
+    html+="<h1>Images</h1>"
+    pics = []
+    pics += glob.glob(folder+"/*.png")
+    pics += glob.glob(folder+"/*.jpg")
+    for pic in sorted(pics):
+        url = os.path.basename(pic)
+        html += f"<a href='{url}'><img src='{url}'></a> "
+    
+    html+="<h1>HTML Files</h1>"
+    for pic in sorted(glob.glob(folder+"/*.html")):
+        url = os.path.basename(pic)
+        html += f"<li><a href='{url}'>{url}</a> "
+    
+    html += "</body></html>"
+    fname = folder+"/index-pics.html"
+    with open(fname, 'w') as f:
+        f.write(html)
+    if launch:
+        webbrowser.open(fname)
+
 
 class InfoPage:
     """
@@ -30,68 +62,78 @@ class InfoPage:
     def showText(self):
         for item in self.things:
             name, value = item
-            if value=="~SECTION~":
-                print("\n### %s ###"%name)
-            elif value=="~DOCS~":
-                print("\n~~~ %s ~~~"%name)
+            if value == "~SECTION~":
+                print("\n### %s ###" % name)
+            elif value == "~DOCS~":
+                print("\n~~~ %s ~~~" % name)
             else:
                 if value is None:
-                    print("%s"%(name))
+                    print("%s" % (name))
                 else:
-                    print("%s = %s"%(name, value))
+                    print("%s = %s" % (name, value))
         return
 
-    def getMarkdown(self):
-        out="# %s\n"%(self.title)
+    def generateMarkdown(self, saveAs=False):
+        out = "# %s\n" % (self.title)
         for item in self.things:
             name, value = item
-            if str(value)=="~SECTION~":
-                out+="\n## %s\n\n"%(name)
-            elif str(value)=="~DOCS~":                    
-                out+="> %s \n\n"%(name.strip().replace("\n"," "))
+            if str(value) == "~SECTION~":
+                out += "\n## %s\n\n" % (name)
+            elif str(value) == "~DOCS~":
+                out += "> %s \n\n" % (name.strip().replace("\n", " "))
             else:
                 if value is None:
-                    out+="* %s\n"%(name)
+                    out += "* %s\n" % (name)
                 else:
-                    out+="* %s = `%s`\n"%(name, str(value).replace("\n"," "))
+                    out += "* %s = `%s`\n" % (name,
+                                              str(value).replace("\n", " "))
+
+        if saveAs:
+            with open(saveAs, 'w') as f:
+                f.write(html)
+
         return out
-        
 
-
-    def getHTML(self):
-        html="<html>"
-        html+="<head>"
-        html+="<style>"
-        html+="body {font-family: sans-serif;}"
-        html+="code {background-color: #F2F2F2; padding: 2px;}"
-        html+=".item {margin-left: 2em; margin-top: .5em;}"
-        html+=".section {font-size: 150%; font-weight: bold; margin-top: 2em; }"
-        html+=".docs {font-style: italic; font-family: serif;}"
-        html+="</style>"
-        html+="<title>%s</title>"%(self.title)
-        html+="</head>"
-        html+="<body>"
-        html+="<h1>%s</h1>"%self.title
+    def generateHTML(self, saveAs=False):
+        html = "<html>"
+        html += "<head>"
+        html += "<style>"
+        html += "body {font-family: sans-serif;}"
+        html += "code {background-color: #F2F2F2; padding: 2px;}"
+        html += ".item {margin-left: 2em; margin-top: .5em;}"
+        html += ".section {font-size: 150%; font-weight: bold; margin-top: 2em; }"
+        html += ".docs {font-style: italic; font-family: serif;}"
+        html += "</style>"
+        html += "<title>%s</title>" % (self.title)
+        html += "</head>"
+        html += "<body>"
+        html += "<h1>%s</h1>" % self.title
         for item in self.things:
             name, value = item
-            if str(value)=="~SECTION~":
-                html+="<div class='section'>%s</div>"%name
-            elif str(value)=="~DOCS~":
-                html+="<div class='docs'>%s</div>"%name.strip().replace("\n","<br>")
+            if str(value) == "~SECTION~":
+                html += "<div class='section'>%s</div>" % name
+            elif str(value) == "~DOCS~":
+                html += "<div class='docs'>%s</div>" % name.strip().replace("\n", "<br>")
             else:
                 if value is None:
-                    html+="<div class='item'>%s</div>"%(name)
+                    html += "<div class='item'>%s</div>" % (name)
                 else:
-                    html+="<div class='item'>%s = <code>%s</code></div>"%(name, value)
-        html+="</body></html>"
+                    html += "<div class='item'>%s = <code>%s</code></div>" % (
+                        name, value)
+        html += "</body></html>"
+
+        if saveAs:
+            with open(saveAs, 'w') as f:
+                f.write(html)
+
         return html
 
     def launchTempWebpage(self):
-        html = self.getHTML()
+        html = self.generateHTML()
         with tempfile.TemporaryDirectory() as tempDir:
             fname = tempDir+"/header.html"
             print(fname)
-            f = open(fname,'w')
+            f = open(fname, 'w')
             f.write(html)
             f.flush()
             webbrowser.open(fname)
@@ -101,21 +143,6 @@ class InfoPage:
 
 if __name__ == "__main__":
 
-    page = InfoPage()
-
-    page.addSection("Methods")
-    page.addDocs("Some cool description of some interesting thing")
-    page.addThing("abf.infoBrowser()")
-    page.addThing("abf.infoDisplay()")
-    page.addThing("abf.setSweep()")
-
-    page.addSection("Variables")
-    page.addThing("abfID", "16d22006_kim_gapfree")
-    page.addThing("abfVersion", 2.123)
-    page.addThing("adcNames", ['IN 2', 'IN 3'])
-
-    #page.launchTempWebpage("tester")
-
-    print(page.getMarkdown())
+    indexFolder(R"C:\Users\scott\Documents\temp")
 
     print("DONE")

tests/2018-06-19 header index update.py
@@ -40,26 +40,45 @@ if __name__ == "__main__":
         page = abf.getInfoPage()
         headerPath = os.path.abspath(PATH_HERE+"/../data/headers/")
         with open(headerPath+"/%s.md" % (abf.abfID), 'w') as f:
-            f.write(page.getMarkdown())
+            f.write(page.generateMarkdown())
         with open(headerPath+"/%s.html" % (abf.abfID), 'w') as f:
-            f.write(page.getHTML())
+            f.write(page.generateHTML())
 
-        # create thumbnail
-        abf.setSweep(abf.sweepCount-1, abf.dataChannelCount-1)
-        plt.figure(figsize=(6, 4))
-        plt.title("{} channel {} sweep {}".format(
-            abf.abfID, abf.sweep.channel+1, abf.sweep.number+1))
-        plt.plot(abf.sweep.x, abf.sweep.y)
-        plt.ylabel(abf.sweep.units)
-        plt.xlabel(abf.sweep.unitsX)
-        plt.margins(0,.1)
-        plt.tight_layout()
-        plt.savefig(headerPath+"/%s.jpg" % (abf.abfID))
+        # create figure and subplots
+        fig = plt.figure(figsize=(8, 6))
+        ax1 = plt.subplot(211)
+        ax2 = plt.subplot(212)
+        ax1.set_xmargin(0)
+        ax2.set_xmargin(0)
+
+        # overlap or continuous view depends on protocol
+        absoluteTime = False
+        if "0111" in abf.protocol:
+            absoluteTime = True
+        if "loose" in abf.protocol:
+            absoluteTime = True
+
+        # plot the data sweep by sweep
+        for sweep in abf.sweepList:
+            abf.setSweep(sweep, absoluteTime=absoluteTime)
+            ax1.plot(abf.sweepX, abf.sweepY, alpha=.5, color='b', lw=.5)
+            ax2.plot(abf.sweepX, abf.sweepC, color='r')
+
+        # decorate plot and save it
+        ax1.set_title("{}.abf [channel: {}/{}] [sweeps: {}]".format(
+            abf.abfID, abf.sweepChannel+1, abf.channelCount, abf.sweepNumber+1))
+        ax1.set_ylabel(abf.sweepLabelY)
+        ax1.set_xlabel(abf.sweepLabelX)
+        ax2.set_ylabel(abf.sweepLabelC)
+        ax2.set_xlabel(abf.sweepLabelX)
+        fig.tight_layout()
+        fig.savefig(PATH_HERE+"/../data/headers/%s.jpg" % abf.abfID)
         plt.close()
+
 
         # update main readme
         md += "[%s.abf](headers/%s.md) (%s)|" % (abf.abfID, abf.abfID, abf.abfVersion)
-        md += "%s (%s)|" % (abf.dataChannelCount, ", ".join(abf.adcUnits))
+        md += "%s (%s)|" % (abf.channelCount, ", ".join(abf.adcUnits))
         md += "%s|" % (abf.sweepCount)
         md += "%s|" % (abf.protocol)
         md += "![headers/%s.jpg](headers/%s.jpg)\n" % (abf.abfID, abf.abfID)
@@ -67,4 +86,7 @@ if __name__ == "__main__":
     # write main readme
     with open(PATH_HERE+"/../data/readme.md", 'w') as f:
         f.write(md)
+
+    # update the file index
+    pyabf.text.indexFolder(PATH_HERE+"/../data/headers/")
     print("DONE")

tests/2018-08-20 test.py
@@ -1,5 +1,5 @@
 """
-Example code demonstrating how to interact with pyabf
+Example code demonstrating how to interact with the pyabf.ABF object
 """
 
 # import the pyabf module from this development folder
@@ -8,20 +8,32 @@ import sys
 PATH_HERE = os.path.abspath(os.path.dirname(__file__))
 PATH_SRC = os.path.abspath(PATH_HERE+"/../src/")
 PATH_DATA = os.path.abspath(PATH_HERE+"/../data/")
-sys.path.insert(0,PATH_SRC) # for importing
-sys.path.append("../src/") # for your IDE
+sys.path.insert(0, PATH_SRC)  # for importing
+sys.path.append("../src/")  # for your IDE
 import pyabf
 pyabf.info()
 import datetime
 import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('bmh')  # alternative color scheme
 
 # now you are free to import additional modules
 import glob
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
-    for fname in sorted(glob.glob(PATH_DATA+"/*.abf")):
+    allFiles = sorted(glob.glob(PATH_DATA+"/*.abf"))
+
+    fnames = []
+    fnames.append(PATH_DATA+"/model_vc_step.abf")  # VC memtest
+    fnames.append(PATH_DATA+"/17o05026_vc_stim.abf")  # VC opto
+    fnames.append(PATH_DATA+"/14o08011_ic_pair.abf")  # IC pair
+    fnames.append(PATH_DATA+"/171116sh_0012.abf")  # VC pair
+
+    outFolder = R"C:\Users\scott\Documents\temp"
+
+    for fname in fnames:
         abf = pyabf.ABF(fname)
         print(abf.abfID)
-        page = abf.getInfoPage()
+
     print("DONE")

commit 2bc0012195f1c1305c0d28eec975e3975f23f2ec

ABFCore: Don't error out on unknown epoch types
The warn method has with python 3.6 the following declaration
warnings.warn(message, category=None, stacklevel=1, source=None)
and therefore the epochType is wrong here.

Add the epoch type to the message instead.

Broken since 9bbf45bd (protocol generation, 2018-06-19).

src/pyabf/core.py
@@ -484,8 +484,7 @@ class ABFcore:
                 self.sweepC[position:position2] = ramp
 
             else:
-                warnings.warn(
-                    "treating unknown epoch type like a step", epochType)
+                warnings.warn("treating unknown epoch type %d like a step" % epochType)
                 self.sweepC[position:position2] = afterDelta
             position += pointCount
 