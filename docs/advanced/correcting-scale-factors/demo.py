df8c4e66a5a5b1c1d15bdcaab0e88bf34be01b5a
Scott W Harden <swharden@gmail.com>
2018-08-11 14:22:56 -0400

    script to modify fInstrumentScaleFactor

--- docs/advanced/correcting-scale-factors/demo.py
+++ docs/advanced/correcting-scale-factors/demo.py
@@ -10,6 +10,10 @@ sys.path.insert(0, PATH_SRC)  # useful for imports
 import pyabf
 import glob
 import shutil
+import struct
+
+BLOCKSIZE = 512
+
 
 class ABFmodify:
     def __init__(self, abfFilePath, backup=True):
@@ -20,29 +24,86 @@ class ABFmodify:
         If backup is enabled, ABFs will be backed-up before modifying them.
         The backup is the original filename with a .abf.backup extension.
         """
-        assert isinstance(abfFilePath,str)
+        assert isinstance(abfFilePath, str)
         self.abfFilePath = os.path.abspath(abfFilePath)
         assert os.path.exists(abfFilePath)
         self.backup = backup
 
     def ensureBackupExists(self):
-        return
+        backupFilePath = self.abfFilePath+".backup"
+        if not os.path.exists(backupFilePath):
+            print("creating", os.path.basename(backupFilePath))
+            shutil.copy(self.abfFilePath, backupFilePath)
 
-    def setScaleFactor(self, fInstrumentScaleFactor):
-        return
+    def bytesWrite(self, bytePosition, byteValues):
+        self.ensureBackupExists()
+        #print("WRITING", byteValues, "at byte", bytePosition)
+        with open(self.abfFilePath, 'r+b') as f:
+            f.seek(bytePosition)
+            f.write(bytes(byteValues))
+
+    def bytesRead(self, bytePosition, byteCount):
+        with open(self.abfFilePath, 'rb') as f:
+            f.seek(bytePosition)
+            values = f.read(byteCount)
+        return values
+
+    def structRead(self, bytePosition, structFormat):
+        byteCount = struct.calcsize(structFormat)
+        byteValues = self.bytesRead(bytePosition, byteCount)
+        values = struct.unpack(structFormat, byteValues)
+        return values
+
+    def getScaleFactor(self):
+        """
+        fInstrumentScaleFactor is a 4-byte float.
+        It's located at the start of each ADCSection + 40 bytes
+        """
+
+        ADCSection = self.structRead(92, "IIl")
+        blockStart, entrySize, entryCount = ADCSection
+        byteStart = blockStart*BLOCKSIZE
+
+        for channel in range(entryCount):
+            bytePosition = byteStart + channel*entrySize + 40
+            byteValues = self.bytesRead(bytePosition, 4)
+            scaleFactorFloat = struct.unpack("f", byteValues)[0]
+            print(os.path.basename(self.abfFilePath), "Ch", channel,
+                  "fInstrumentScaleFactor =", list(byteValues), "=",
+                  scaleFactorFloat)
+
+    def setScaleFactor(self, byteVals=[], channel=0):
+        """
+        fInstrumentScaleFactor is a 4-byte float.
+        It's located at the start of each ADCSection + 40 bytes
+        """
+
+        assert isinstance(byteVals, list)
+        assert len(byteVals) == 4
+
+        ADCSection = self.structRead(92, "IIl")
+        blockStart, entrySize, entryCount = ADCSection
+        byteStart = blockStart*BLOCKSIZE
+        bytePosition = byteStart + channel*entrySize + 40
+        self.bytesWrite(bytePosition, byteVals)
+
 
 if __name__ == "__main__":
     #abf_file_folder = R"X:\Data\C57\BLA injection microspheres\technique development\abfs"
     abf_file_folder = PATH_HERE+"/demoABFs/"
     abfFiles = glob.glob(abf_file_folder+"/*.abf")
 
-    for abfNumber,fname in enumerate(sorted(abfFiles)):
-        print(f"Analyzing ({abfNumber}/{len(abfFiles)}) {os.path.basename(fname)}", end=" ... ")
+    for abfNumber, fname in enumerate(sorted(abfFiles)):
+        msg = f"Analyzing ({abfNumber}/{len(abfFiles)})"
+        print(f"{msg} {os.path.basename(fname)}", end=" ... ")
+
         abf = pyabf.ABF(fname, preLoadData=False)
 
         # ensure units arent the same
         if abf.adcUnits[0] == abf.dacUnits[0]:
-            print("ERROR!!!",abf.adcUnits[0],abf.dacUnits[0])
+            print("ERROR ... ", end="")
             abfMod = ABFmodify(abf.abfFilePath)
+            abfMod.setScaleFactor([10, 215, 35, 60])
+            print("FIXED!")
         else:
             print("OK")
