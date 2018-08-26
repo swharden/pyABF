4ae95e19b21084d26b05c20ae562924ab620e16e
Scott W Harden <swharden@gmail.com>
2018-08-26 16:00:23 -0400

    port to cs

---
+++ dev/python/2018-08-26 port cs.py
@@ -0,0 +1,42 @@
+"""
+code here helps port this code to csharp
+"""
+
+import os
+import sys
+PATH_HERE = os.path.abspath(os.path.dirname(__file__))
+PATH_SRC = os.path.abspath(PATH_HERE+"../../../src/")
+
+if __name__=="__main__":
+    out=""
+    with open(PATH_SRC+"/pyabf/abfHeader.py") as f:
+        raw=f.read().split("\n")
+    for line in raw:
+        line=line.strip()
+        if line.startswith("def"):
+            out+="\n"
+        if not line.startswith("self."):
+            continue
+        if not "readStruct(" in line:
+            continue
+        if "#" in line:
+            line=line.split("#")[0]
+        line=line.replace("'",'"')
+        if '"f"' in line:
+            line=line.replace("self.","float ")
+        elif '"h"' in line:
+            line=line.replace("self.","short ")
+        elif '"i"' in line:
+            line=line.replace("self.","int ")
+        elif '"I"' in line:
+            line=line.replace("self.","unsigned int ")
+        elif '"b"' in line:
+            line=line.replace("self.","signed char ")
+        elif '"c"' in line:
+            line=line.replace("self.","char ")
+        else:
+            line=line.replace("self.","var ")
+        out+=line+"\n"
+    with open(PATH_HERE+"/2018-08-26 port cs.cs",'w') as f:
+        f.write(out)
+    print("DONE")
