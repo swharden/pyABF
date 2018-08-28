5404db9cc1d954513bc0c156673f72c34db67440
Scott W Harden <swharden@gmail.com>
2018-08-28 14:05:06 -0400

    capacitance calculation demo with memtest module

---
+++ dev/python/2018-08-28 memtest module.py
@@ -0,0 +1,23 @@
+"""
+demonstrate how to use the new pyabf.memtest module
+"""
+
+import os
+import sys
+PATH_HERE = os.path.abspath(os.path.dirname(__file__))
+PATH_DATA = os.path.abspath(PATH_HERE+"../../../data/abfs/")
+PATH_SRC = os.path.abspath(PATH_HERE+"../../../src/")
+sys.path.insert(0, PATH_SRC)
+import pyabf
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__=="__main__":
+    modelRamp = PATH_DATA+"/model_vc_ramp.abf"
+    modelStep = PATH_DATA+"/model_vc_step.abf"
+
+    abf=pyabf.ABF(modelRamp)
+    print(pyabf.memtest.cm_ramp_summary(abf))
+
+    print("DONE")
