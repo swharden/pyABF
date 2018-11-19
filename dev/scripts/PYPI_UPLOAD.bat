727cbbb69de43a86fce88b0aac45040b166544c8
Scott W Harden <swharden@gmail.com>
2018-11-19 10:35:04 -0500

    improve pypi upload script

--- dev/scripts/PYPI_UPLOAD.bat
+++ dev/scripts/PYPI_UPLOAD.bat
@@ -7,9 +7,25 @@
 :: make commands silent
 @echo off
 
-:: activate anaconda
+:: activate anaconda (home)
+echo.
+echo activating Anaconda in user folder...
 set root=C:\%HOMEPATH%\Anaconda3
 call %root%\Scripts\activate.bat %root%
+
+:: activate anaconda (work)
+echo.
+echo activating Anaconda in AppData...
+set root=%HOMEPATH%\AppData\Local\Continuum\Anaconda3
+call %root%\Scripts\activate.bat %root%
+
+echo.
+echo testing activation by running conda...
+call conda -V
+
+echo.
+echo press ENTER to continue if one activation worked!
+PAUSE
 
 :: perform a version increase
 python versionIncrease.py
@@ -28,12 +44,12 @@ echo building distribution with setuptools...
 python setup.py --quiet sdist
 
 :: upload to the test server
-twine upload --username swharden --repository-url https://test.pypi.org/legacy/ dist/*
-explorer https://test.pypi.org/project/pyabf/
+::twine upload --username swharden --repository-url https://test.pypi.org/legacy/ dist/*
+::explorer https://test.pypi.org/project/pyabf/
 
 :: upload to real server
-echo CONTINUE TO UPLOAD TO REAL PYPI
-twine upload --username swharden dist/*
+::echo CONTINUE TO UPLOAD TO REAL PYPI
+twine upload --username swharden --repository-url https://upload.pypi.org/legacy/ dist/*
 explorer https://pypi.org/project/pyabf/
 
 :: delete old builds
