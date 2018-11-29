001f9ed318ddc3a5f84d1a02ab3d1dcf0654a9b0
Scott W Harden <swharden@gmail.com>
2018-11-28 21:40:24 -0500

    remove Python 2.7 version
    now that Python 2.7 is natively supported

--- dev/python27_version/pyabf2/__init__.py
+++
@@ -1,5 +0,0 @@
-"""This is a stripped-down version of pyABF designed to run on python 2.x"""
-import sys
-if sys.version_info >= (3,0):
-    print("ERROR: this pyABF module is intended just for python 2.x")
-from pyabf2.abf import ABF
