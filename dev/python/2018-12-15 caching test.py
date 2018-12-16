ba7af95fabebc31dcf0c890bad6b5c2442605d4f
Scott W Harden <swharden@gmail.com>
2018-12-15 20:52:30 -0500

    improve caching test

--- dev/python/2018-12-15 caching test.py
+++ dev/python/2018-12-15 caching test.py
@@ -39,6 +39,10 @@ assert os.path.exists(testDirectory)
 
 def setup_test_directory():
     """Create many ABF files which use ABF and ATF waveforms."""
+    print("deleting old ABFs...")
+    for fname in glob.glob(os.path.join(testDirectory, "cool_*.abf")):
+            os.remove(fname)
+    print("copying ABFs...")
     for i in range(5):
         fnameABF = os.path.join(PATH_DATA, "171116sh_0015.abf")
         fnameATF = os.path.join(PATH_DATA, "171116sh_0015-ATFwaveform.abf")
@@ -52,15 +56,27 @@ def test_stimulus_cache():
     abfFiles = sorted(glob.glob(testDirectory+"/cool_*.abf"))
     for i, filename in enumerate(abfFiles):
         abf = pyabf.ABF(filename)
-        abf.stimulusByChannel[0].protocolStorageDir = PATH_DATA
+
+        # indicate that stimulus files may be in an alternate folder
+        abf.stimulusFileFolder = PATH_DATA # new way
+        #abf.stimulusByChannel[0].protocolStorageDir = PATH_DATA # old way
+
         t1 = time.perf_counter()
         sweepC = abf.sweepC # stimulus file is only read if this is accessed
         t2 = time.perf_counter()
         stimFile = os.path.basename(abf._stringsIndexed.lDACFilePath[0])
         timeMs = (t2 - t1)*1000.0
-        print("%s loaded in %.02f ms"%(stimFile, timeMs))
+        print('"%s" loaded in %.02f ms'%(stimFile, timeMs), sweepC)
+
+def test_stimulus_cache_old():
+        """Test warning if access using old methods"""
+        abfFiles = sorted(glob.glob(testDirectory+"/cool_*.abf"))
+        abf = pyabf.ABF(abfFiles[0])
+        print(abf.stimulusByChannel[0].protocolStorageDir)
+        abf.stimulusByChannel[0].protocolStorageDir = PATH_DATA
 
 if __name__=="__main__":
     setup_test_directory()
+    test_stimulus_cache_old()
     test_stimulus_cache()
     print("DONE")
