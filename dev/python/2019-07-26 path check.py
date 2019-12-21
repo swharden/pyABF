0b7f5b8b246665c6e6175d180716799824bf6ac2
Scott W Harden <swharden@gmail.com>
2019-12-21 18:40:58 -0500

    update path check demo script

--- dev/python/2019-07-26 path check.py
+++ dev/python/2019-07-26 path check.py
@@ -8,7 +8,8 @@ sys.path.insert(0, PATH_SRC)
 import pyabf
 
 if __name__ == "__main__":
-    #pyabf.stimulus.Stimulus.protocolStorageDir = "/local1/ephys/abf_convertion/reference_atf"
-    abf = pyabf.ABF(PATH_DATA+"/H19_29_150_11_21_01_0011.abf")
-    abf.stimulusFileFolder = "/local1/ephys/abf_convertion/reference_atf"
+    abf = pyabf.ABF(
+        abfFilePath=PATH_DATA+"/H19_29_150_11_21_01_0011.abf",
+        stimulusFileFolder="/somewhere/special"
+    )
     print(abf.sweepC)
