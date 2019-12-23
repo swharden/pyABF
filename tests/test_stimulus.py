cf769ababd1946db79e2d0ac4f3593a7b9280793
Scott W Harden <swharden@gmail.com>
2019-12-23 17:34:47 -0500

    add test for caching
    fixes #99

--- tests/test_stimulus.py
+++ tests/test_stimulus.py
@@ -8,6 +8,7 @@ import sys
 import pytest
 import os
 import numpy as np
+import time
 
 try:
     # this ensures pyABF is imported from this specific path
@@ -42,3 +43,37 @@ def test_findStimulusFile_foundIfPathGiven():
     assert isinstance(waveform, np.ndarray)
     assert not np.isnan(waveform).any()
     assert pytest.approx(waveform[100000], 76.261)
+
+
+def cachedStimulusSpeedBoost(useCaching):
+    """Open an ABF/stimulus twice and return the times (in sec)"""
+
+    times = [None, None]
+    useCaching = [False, useCaching]
+    for i in range(2):
+        t1 = time.perf_counter()
+        abf = pyabf.ABF(
+            ABF_PATH,
+            stimulusFileFolder=STIM_FOLDER,
+            cacheStimulusFiles=useCaching[i]
+        )
+        stimulus = abf.stimulusByChannel[0]
+        waveform = stimulus.stimulusWaveform(stimulusSweep=0)
+        assert pytest.approx(waveform[100000], 76.261)
+        times[i] = time.perf_counter() - t1
+
+    speedBoost = times[0]/times[1]
+    print(f"Caching: {useCaching[1]}, speed boost: {speedBoost}x")
+    return speedBoost
+
+
+def test_stimulus_caching():
+
+    # first try without caching
+    assert (cachedStimulusSpeedBoost(False) < 2)
+
+    # now use caching for a >10x speed boost
+    assert (cachedStimulusSpeedBoost(True) > 10)
+
+    # confirm not using caching is still slow
+    assert (cachedStimulusSpeedBoost(False) < 2)
