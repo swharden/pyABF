5d23391bb565488b39b18f60e478c79c893c0c4d
Scott W Harden <swharden@gmail.com>
2020-01-16 20:11:58 -0500

    Update 2020-01-16 guid2.py

--- dev/python/2020-01-16 guid2.py
+++ dev/python/2020-01-16 guid2.py
@@ -6,6 +6,10 @@ import glob
 import os
 import sys
 import matplotlib.pyplot as plt
+import uuid
+import datetime
+import time
+import hashlib
 
 try:
     PATH_HERE = os.path.abspath(os.path.dirname(__file__))
@@ -18,13 +22,14 @@ except:
     raise EnvironmentError()
 
 
-if __name__ == "__main__":
+def countDuplicateGuids(useNewGuid=False):
     guids = []
     guidAndIDs = []
     for abfFilePath in glob.glob(DATA_FOLDER + "/*.abf"):
         abf = pyabf.ABF(abfFilePath)
-        guidAndIDs.append([abf.fileGUID, abf.abfID])
-        guids.append(abf.fileGUID)
+        guid = abf.fileUUID if useNewGuid else abf.fileGUID
+        guidAndIDs.append([guid, abf.abfID])
+        guids.append(guid)
 
     duplicates = 0
     for guid, abfID in sorted(guidAndIDs):
@@ -35,3 +40,12 @@ if __name__ == "__main__":
             print(f"{guid}  (unique)  {abfID}.abf")
 
     print(f"Found {duplicates} duplicates out of {len(guidAndIDs)} ABFs")
+
+
+if __name__ == "__main__":
+    #countDuplicateGuids(True)
+
+    abf = pyabf.ABF(PATH_DATA+"/2019_07_24_0055_fsi.abf")
+    print(abf.fileGUID)
+    print(abf.fileUUID)
+    print(abf.md5)
