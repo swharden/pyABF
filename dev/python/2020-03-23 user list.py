42a6333cea027ee19b4989a0938f94a953dad70b
Scott W Harden <swharden@gmail.com>
2020-03-23 22:02:46 -0400

    update demo to use abf.userList #105

--- dev/python/2020-03-23 user list.py
+++ dev/python/2020-03-23 user list.py
@@ -2,7 +2,6 @@
 Invent a better (truly unique) GUID system
 """
 
-import pyabf
 import glob
 import os
 import sys
@@ -22,31 +21,11 @@ try:
 except:
     raise EnvironmentError()
 
-
-def GetUserList(abf):
-    """
-    Return the ABF user list as a list of values, 
-    or return None if the ABF has no user list.
-    """
-    assert isinstance(abf, pyabf.ABF)
-    if not hasattr(abf, '_stringsSection'):
-        return None
-    firstBlock = abf._stringsSection.strings[0]
-    firstBlockStrings = firstBlock.split(b'\x00')
-    userList = firstBlockStrings[-2].decode("utf-8")
-    userList = userList.split(",")
-    try:
-        return [float(x) for x in userList if x]
-    except:
-        return None
-
-
 if __name__ == "__main__":
 
     print("ABF | User List")
     print("----|----------")
     for abfFilePath in glob.glob(PATH_DATA+"/*.abf"):
         abf = pyabf.ABF(abfFilePath)
-        userList = GetUserList(abf)
-        if (userList):
-            print(f"{abf.abfID}|`{userList}`")
+        if (abf.userList):
+            print(f"{abf.abfID}|`{abf.userList}`")
