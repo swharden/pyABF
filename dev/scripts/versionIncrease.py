f56f8f33b898f42a0b8b07dce2d0c74a0a9015b2
Scott W Harden <swharden@gmail.com>
2020-09-03 13:24:19 -0400

    Update versionIncrease.py
    now writes discrete version numbers in setup.py and __init__.py #109

--- dev/scripts/versionIncrease.py
+++ dev/scripts/versionIncrease.py
@@ -2,20 +2,36 @@
 
 import os
 
+
+def nextVersionString(oldVersionString):
+    oldVersion = oldVersionString.split(".")
+    assert len(oldVersion) == 3
+    oldVersion = [int(x) for x in oldVersion]
+    return "%d.%d.%d" % (oldVersion[0], oldVersion[1], oldVersion[2] + 1)
+
+
 def increaseVersion(fname):
     assert os.path.exists(fname)
+
     with open(fname) as f:
-        oldVersionString = f.read().strip()
-    oldVersion = oldVersionString.split(".")
-    oldVersion = [int(x) for x in oldVersion]
-    newVersionString = "%d.%d.%d" % (oldVersion[0], oldVersion[1], oldVersion[2] + 1)	
+        lines = f.read().split("\n")
+
+    for i, line in enumerate(lines):
+        strippedLine = line.replace(" ", "")
+        if strippedLine.startswith("version=") or strippedLine.startswith("__version__="):
+            oldVersion = line.split("'")[1]
+            newVersion = nextVersionString(oldVersion)
+            print(f"{os.path.basename(fname)}\t{oldVersion} -> {newVersion}")
+            lines[i] = line.replace(oldVersion, newVersion)
+
     with open(fname, 'w') as f:
-        f.write(newVersionString)
-    print(f"Upgraded: {oldVersionString} -> {newVersionString}")
-    return
+        f.write("\n".join(lines))
+
+    return newVersion
 
 
 if __name__ == "__main__":
     PATH_HERE = os.path.abspath(os.path.dirname(__file__))
-    versionFile = os.path.abspath(PATH_HERE+"/../../src/pyabf/version.txt")
-    increaseVersion(versionFile)
+    setupVersion = increaseVersion(PATH_HERE+"/../../src/setup.py")
+    pyAbfVersion = increaseVersion(PATH_HERE+"/../../src/pyabf/__init__.py")
+    assert setupVersion == pyAbfVersion
