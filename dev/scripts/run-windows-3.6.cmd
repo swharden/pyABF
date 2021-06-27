c96f5631abc4f4d2b044f98004f4b7d451de70c0
Scott W Harden <swharden@gmail.com>
2021-06-27 17:28:52 -0400

    dev: script to run tests with python 3.6.0

--- dev/scripts/run-windows-3.6.cmd
+++ dev/scripts/run-windows-3.6.cmd
@@ -1,2 +1,2 @@
 ::%LOCALAPPDATA%\Programs\Python\Python36\python.exe -m pip install matplotlib pytest
-%LOCALAPPDATA%\Programs\Python\Python36\python.exe "dev\python\2021-06-27 all atfs.py"
+%LOCALAPPDATA%\Programs\Python\Python36\python.exe "dev\python\2021-06-27 all abfs.py"
