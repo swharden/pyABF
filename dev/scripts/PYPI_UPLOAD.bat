a8b347d8a688c9d1479935d7fe4d87f5708cfb91
Scott W Harden <swharden@gmail.com>
2021-06-27 21:07:49 -0400

    devops: update build script

--- dev/scripts/PYPI_UPLOAD.bat
+++ dev/scripts/PYPI_UPLOAD.bat
@@ -1,43 +1,26 @@
-:: this upload script when Anaconda is installed in the user folder.
-
-:: to use twine I had to first set it up
-:: conda install twine
-:: conda update pip setuptools twine
-
-:: make commands silent
+:: To use this script: pip install twine
 @echo off
 
+:: python versionIncrease.py
 
-:: perform a version increase
-python versionIncrease.py
-
-:: navigate to the source folder
+:: navigate to the source folder and delete old builds
 cd ../../src/
-
-:: delete old builds
 rmdir /S /q _build 2>nul
 rmdir /S /q build 2>nul
 rmdir /S /q dist 2>nul
 rmdir /S /q pyabf.egg-info 2>nul
  
-:: create your distribution
-echo building distribution with setuptools...
+:: create the distribution
+echo ### building distribution with setuptools...
 python setup.py --quiet sdist
 
-:: upload to the test server
-::twine upload --username swharden --repository-url https://test.pypi.org/legacy/ dist/*
-::explorer https://test.pypi.org/project/pyabf/
+echo ### press ENTER 3 times to upload...
+pause
+pause
+pause
 
-:: upload to real server
-::echo CONTINUE TO UPLOAD TO REAL PYPI
 twine upload --username swharden --repository-url https://upload.pypi.org/legacy/ dist/*
 explorer https://pypi.org/project/pyabf/
-
-:: delete old builds
-rmdir /S /q _build 2>nul
-rmdir /S /q build 2>nul
-rmdir /S /q dist 2>nul
-rmdir /S /q pyabf.egg-info 2>nul
 
 echo COMPLETE
 pause
